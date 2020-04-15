0e358a68b604fad4ae0c674bab3e4d86861c66dc
Sonya Cirlos <48666300+sccirlos@users.noreply.github.com>
2020-04-15 09:12:02 -0500

    Create script.js

---
+++ CSS, Website layout + JS/script.js
@@ -0,0 +1,52 @@
+let img1 = document.querySelector("#img1");
+
+img1.addEventListener("click", e => {
+    img1.classList.toggle("active");
+});
+
+let img2 = document.querySelector("#img2");
+
+img2.addEventListener("click", e => {
+    img2.classList.toggle("active");
+});
+
+let img3 = document.querySelector("#img3");
+
+img3.addEventListener("click", e => {
+    img3.classList.toggle("active");
+});
+
+let img4 = document.querySelector("#img4");
+
+img4.addEventListener("click", e => {
+    img4.classList.toggle("active");
+});
+
+let img5 = document.querySelector("#img5");
+
+img5.addEventListener("click", e => {
+    img5.classList.toggle("active");
+});
+
+let img6 = document.querySelector("#img6");
+
+img6.addEventListener("click", e => {
+    img6.classList.toggle("active");
+});
+let img7 = document.querySelector("#img7");
+
+img7.addEventListener("click", e => {
+    img7.classList.toggle("active");
+});
+
+let img8 = document.querySelector("#img8");
+
+img8.addEventListener("click", e => {
+    img8.classList.toggle("active");
+});
+
+let img9 = document.querySelector("#img9");
+
+img9.addEventListener("click", e => {
+    img9.classList.toggle("active");
+});
