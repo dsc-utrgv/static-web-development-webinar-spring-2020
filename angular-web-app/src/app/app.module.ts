ad2c837ba38565e8c87936108a9d741c5165250d
Subba <Subba@gmail.com>
2020-04-29 16:59:19 -0500

    html included

--- angular-web-app/src/app/app.module.ts
+++ angular-web-app/src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import{HttpClientModule} from '@angular/common/http';
+import{FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router' 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: '', component: AppComponent }
+]
 
 @NgModule({
   declarations: [
@@ -10,7 +20,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    // NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
