a0c22762a80404736732b5f8e56b2131718fee7a
Subba <Subba@gmail.com>
2020-04-29 18:35:45 -0500

    final changes

--- angular-web-app/src/app/app.component.ts
+++ angular-web-app/src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-web-app';
+  helloMsg = "Hi My name is Subba!"
+  label_name = "My First Name";
+  credentials: any = {
+    id: 0,
+    first_name: "",
+    last_name: "",
+    email: "subba@g.com",
+    password: "jndjbcdbcdj"
+  };
 
-  register(){}
+  ngOnInit(){
+
+  }
+
+  register(){
+    console.log(this.credentials)
+  }
 }
